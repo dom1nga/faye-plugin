47fd21fc370be7b889911b635f234aa1cdaf8607
Alexander Brodianoy <agb@anahoret.com>
2011-11-03 13:41:48 +0200

    Helper methods works now.

--- lib/faye-plugin.rb
+++ lib/faye-plugin.rb
@@ -1,4 +1,26 @@
 module FayePlugin
-  autoload :Helpers, 'faye-plugin/helpers'
-  autoload :Observers, 'faye-plugin/observers'
+
+  def self.included(base)
+    base.extend ClassMethods
+    base.send :include, InstanceMethods
+  end
+
+  module ClassMethods
+  end
+
+  module InstanceMethods
+
+    def faye_watcher(object, attribute)
+      content_tag(:div, "#{object[attribute.to_sym]}", {:id => "#{object.class.name.downcase}_#{object.id}_#{attribute}"})
+    end
+
+    def broadcast(channel, &block)
+      message = {:channel => channel, :data => capture(&block), :ext => {:auth_token => FAYE_TOKEN}}
+      uri = URI.parse("http://#{Rails.configuration.faye_host}:#{Rails.configuration.faye_port}/faye")
+      Net::HTTP.post_form(uri, :message => message.to_json)
+    end
+
+  end
 end
+
+ActionView::Base.send(:include, FayePlugin) if defined?(ActionView::Base)
