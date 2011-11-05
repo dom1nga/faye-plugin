0627c98e54cc4119b08ae60a31069c61f10d2b61
dom1nga <beloved.dom1nga@gmail.com>
2011-11-05 13:07:34 +0200

    Changed broadcast helper.

--- lib/faye-plugin.rb
+++ lib/faye-plugin.rb
@@ -14,10 +14,8 @@ module FayePlugin
       content_tag(:div, "#{object[attribute.to_sym]}", {:id => "#{object.class.name.downcase}_#{object.id}_#{attribute}"})
     end
 
-    def broadcast(channel, &block)
-      message = {:channel => channel, :data => capture(&block), :ext => {:auth_token => FAYE_TOKEN}}
-      uri = URI.parse("http://#{Rails.configuration.faye_host}:#{Rails.configuration.faye_port}/faye")
-      Net::HTTP.post_form(uri, :message => message.to_json)
+    def broadcast(&block)
+      @faye_client.publish '/listener', :data => capture(&block)
     end
 
   end
