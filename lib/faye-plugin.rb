0371e14085d9f87d1949c06b9eceff697bfc0673
dom1nga <beloved.dom1nga@gmail.com>
2011-11-06 03:27:46 +0200

    Added controller methods.

--- lib/faye-plugin.rb
+++ lib/faye-plugin.rb
@@ -1,24 +1,44 @@
 module FayePlugin
 
-  def self.included(base)
-    base.extend ClassMethods
-    base.send :include, InstanceMethods
-  end
+  module HelperAdditions
 
-  module ClassMethods
-  end
-
-  module InstanceMethods
-
-    def faye_watcher(object, attribute)
-      content_tag(:div, "#{object[attribute.to_sym]}", {:id => "#{object.class.name.downcase}_#{object.id}_#{attribute}"})
+    def self.included(base)
+      base.send :include, InstanceMethods
     end
 
-    def broadcast(&block)
-      @faye_client.publish '/listener', :data => capture(&block)
+    module InstanceMethods
+      def faye_watcher(object, attribute)
+        content_tag(:div, "#{object[attribute.to_sym]}", {:id => "#{object.class.name.downcase}_#{object.id}_#{attribute}"})
+      end
+
+      def broadcast(&block)
+        @faye_client.publish '/listener', :data => capture(&block)
+      end
     end
 
   end
+
+  module ControllerAdditions
+
+    def self.included(base)
+      base.send :include, InstanceMethods
+    end
+
+    module InstanceMethods
+
+      private
+
+      def find_faye_client
+          @faye_client = env['faye.client']
+      end
+
+      def set_faye_client_for_observer
+        ActionObserver.faye_client = env['faye.client']
+      end
+    end
+  end
+
 end
 
-ActionView::Base.send(:include, FayePlugin) if defined?(ActionView::Base)
+ActionView::Base.send(:include, FayePlugin::HelperAdditions) if defined?(ActionView::Base)
+ActionController::Base.class_eval { include FayePlugin::ControllerAdditions } if defined? ActionController
